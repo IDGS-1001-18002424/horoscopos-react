d4d135f649752d93c5817b5c78981dd2d522fcb8
IDGS-1001-18002424 <18002424@alumnos.utleon.edu.mx>
2023-10-16 09:44:20 -0600

    plugin Reload

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i9jugh5kaoo"
+    "revision": "0.p46shl9jalg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
